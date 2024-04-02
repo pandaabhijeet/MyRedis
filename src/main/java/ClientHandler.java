2c10601999f83ec772371cd3415c5f5b2e8878e9
Abhijeet Panda <abhijeetpanda06@gmail.com>
2024-04-02 09:50:05 +0530

    added multiple client handler

---
+++ src/main/java/ClientHandler.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
+public class ClientHandler implements Runnable {
+
+    private final Socket clientSocket;
+
+    public ClientHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            BufferedReader inReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            OutputStream outputStream = clientSocket.getOutputStream();
+            String input;
+            String response = "+PONG\r\n";
+            while ((input = inReader.readLine()) != null) {
+                System.out.println(input);
+
+                if (input.equals("ping")) {
+                    outputStream.write(response.getBytes(Charset.defaultCharset()));
+                }
+
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
+        }
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,54 +1,36 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.Charset;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
+  public static void main(String[] args) {
+    // You can use print statements as follows for debugging, they'll be visible
+    // when running tests.
     System.out.println("Logs from your program will appear here!");
 
-     //Uncomment this block to pass the first stage
-       ServerSocket serverSocket = null;
-       Socket clientSocket = null;
-       int port = 6379;
-       String response = "+PONG\r\n";
-      
-       try {
-         serverSocket = new ServerSocket(port);
-         // Since the tester restarts your program quite often, setting SO_REUSEADDR
-         // ensures that we don't run into 'Address already in use' errors
-         serverSocket.setReuseAddress(true);
-         // Wait for connection from client.
-         clientSocket = serverSocket.accept();
+    // Uncomment this block to pass the first stage
+    ServerSocket serverSocket = null;
+    Socket clientSocket;
+    int port = 6379;
+    ExecutorService executorService = Executors.newCachedThreadPool();
 
-         //clientSocket.getOutputStream().write(response.getBytes(Charset.defaultCharset()));
-
-         BufferedReader inReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-         OutputStream os = clientSocket.getOutputStream();
-         String input;
-         while ((input = inReader.readLine()) != null) {
-         System.out.println(input);
-
-          if(input.equals("ping")){
-            os.write(response.getBytes(Charset.defaultCharset()));
-          }
-          
-         } 
-
-       } catch (IOException e) {
-         System.out.println("IOException: " + e.getMessage());
-       } finally {
-         try {
-           if (clientSocket != null) {
-             clientSocket.close();
-           }
-         } catch (IOException e) {
-           System.out.println("IOException: " + e.getMessage());
-         }
-       }
+    try {
+      serverSocket = new ServerSocket(port);
+      // Since the tester restarts your program quite often, setting SO_REUSEADDR
+      // ensures that we don't run into 'Address already in use' errors
+      serverSocket.setReuseAddress(true);
+      // Wait for connection from client.
+      // clientSocket.getOutputStream().write(response.getBytes(Charset.defaultCharset()));
+      while (!serverSocket.isClosed()) {
+        clientSocket = serverSocket.accept();
+        executorService.submit(new ClientHandler(clientSocket));
+      }
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
+    } finally {
+      executorService.shutdown();
+    }
   }
 }
