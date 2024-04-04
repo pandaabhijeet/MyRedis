598cde3c0f22d64b2d9f9bb1106e0ba8316e6d1c
Abhijeet Panda <abhijeetpanda06@gmail.com>
2024-04-04 09:51:36 +0530

    adding support for ECHO command

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.charset.Charset;
+import java.util.List;
 
 public class ClientHandler implements Runnable {
 
@@ -16,17 +17,34 @@ public class ClientHandler implements Runnable {
     @Override
     public void run() {
         try {
-            BufferedReader inReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            //BufferedReader inReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             OutputStream outputStream = clientSocket.getOutputStream();
-            String input;
+            //String input;
             String response = "+PONG\r\n";
-            while ((input = inReader.readLine()) != null) {
-                System.out.println(input);
+            // while ((input = inReader.readLine()) != null) {
+            //     System.out.println(input);
 
-                if (input.equals("ping")) {
+            //     if (input.equals("ping")) {
+            //         outputStream.write(response.getBytes(Charset.defaultCharset()));
+            //     }
+
+            // }
+
+            while (true) {
+                RESPCommandParser respCommandParser = new RESPCommandParser();
+                List<String> respCommands = respCommandParser.readRespCommands(clientSocket.getInputStream());
+
+                if (respCommands.isEmpty()) {
+                    System.out.println("Empty command");
+                    continue;
+                }
+                if (respCommands.get(0).equalsIgnoreCase("PING")){
                     outputStream.write(response.getBytes(Charset.defaultCharset()));
                 }
 
+                if(respCommands.get(0).equalsIgnoreCase("ECHO")){
+                    outputStream.write(respCommands.get(1).getBytes(Charset.defaultCharset()));
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
---
+++ src/main/java/RESPCommandParser.java
@@ -0,0 +1,57 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RESPCommandParser {
+    public List<String> readRespCommands(InputStream inputStream){
+
+        //*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n
+
+        List<String> respCommands = new ArrayList<>();
+        int b = inputStream.read();
+
+        while(true){
+            char type = (char) b;
+
+            switch (type) {
+                case '*':
+                    StringBuilder numCommandBuilder = new StringBuilder();
+                    while((b = inputStream.read()) != '\r'){
+                        numCommandBuilder.append((char) b);
+                    }
+
+                    inputStream.read();
+
+                    int numCommands = Integer.parseInt(numCommandBuilder.toString());
+
+                    for(int i=0; i < numCommands; i++){
+                        StringBuilder componentString = new StringBuilder();
+                        inputStream.read();
+
+                        while ((b = inputStream.read()) != '\r') {
+                            componentString.append((char) b);
+                        }
+                        inputStream.read();
+
+                        int numComponentStr = Integer.parseInt(componentString.toString());
+                        StringBuilder finalStr = new StringBuilder();
+
+                        for(int j =0; j<numComponentStr; j++){
+                            finalStr.append((char)inputStream.read());
+                        }
+
+                        respCommands.add(finalStr.toString());
+                        inputStream.read();
+                        inputStream.read();
+                    }
+
+                return respCommands;    
+                default:
+                    throw new IOException("Invalid protocol: " +type);
+            }
+        }
+    } 
+}
+
+
