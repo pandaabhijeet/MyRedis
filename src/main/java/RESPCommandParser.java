239ec623741fca6e3573db88c5236a41fd12a789
Abhijeet Panda <abhijeetpanda06@gmail.com>
2024-04-04 10:17:48 +0530

    adding support for ECHO command

--- src/main/java/RESPCommandParser.java
+++ src/main/java/RESPCommandParser.java
@@ -4,59 +4,57 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RESPCommandParser {
-    public List<String> readRespCommands(InputStream inputStream){
+    public List<String> readRespCommands(InputStream inputStream) {
 
-        //*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n
+        // *2\r\n$5\r\nhello\r\n$5\r\nworld\r\n
 
         List<String> respCommands = new ArrayList<>();
         int b = inputStream.read();
 
-        while(true){
-            char type = (char) b;
+        try {
+            while (true) {
+                char type = (char) b;
 
-            try{
                 switch (type) {
                     case '*':
                         StringBuilder numCommandBuilder = new StringBuilder();
-                        while((b = inputStream.read()) != '\r'){
+                        while ((b = inputStream.read()) != '\r') {
                             numCommandBuilder.append((char) b);
                         }
-    
+
                         inputStream.read();
-    
+
                         int numCommands = Integer.parseInt(numCommandBuilder.toString());
-    
-                        for(int i=0; i < numCommands; i++){
+
+                        for (int i = 0; i < numCommands; i++) {
                             StringBuilder componentString = new StringBuilder();
                             inputStream.read();
-    
+
                             while ((b = inputStream.read()) != '\r') {
                                 componentString.append((char) b);
                             }
                             inputStream.read();
-    
+
                             int numComponentStr = Integer.parseInt(componentString.toString());
                             StringBuilder finalStr = new StringBuilder();
-    
-                            for(int j =0; j<numComponentStr; j++){
-                                finalStr.append((char)inputStream.read());
+
+                            for (int j = 0; j < numComponentStr; j++) {
+                                finalStr.append((char) inputStream.read());
                             }
-    
+
                             respCommands.add(finalStr.toString());
                             inputStream.read();
                             inputStream.read();
                         }
-    
-                    return respCommands;    
+
+                        return respCommands;
                     default:
-                        throw new IOException("Invalid protocol: " +type);
+                        throw new IOException("Invalid protocol: " + type);
                 }
-            }catch(IOException e){
-                throw new IOException(e);
+
             }
-            
+        } catch (IOException e) {
+            throw new IOException(e);
         }
-    } 
+    }
 }
-
-
